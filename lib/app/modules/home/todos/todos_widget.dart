7454d8cfcb09aba2a460a25221964df3dada2d48
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2022-04-23 20:53:48 -0300

    Creating a form

--- lib/app/modules/home/todos/todos_widget.dart
+++ lib/app/modules/home/todos/todos_widget.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TodosWidget extends StatelessWidget {
+class TodosWidget extends StatefulWidget {
   const TodosWidget({Key? key}) : super(key: key);
+
+  @override
+  State<TodosWidget> createState() => _TodosWidgetState();
+}
+
+class _TodosWidgetState extends State<TodosWidget> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        Center(
-          child: Text('Todos'),
+      children: [
+        Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Processing data...'),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
         ),
       ],
     );
