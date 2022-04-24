c6340742977e3d6cd44e589604fb69c6842cce3b
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2022-04-23 21:16:44 -0300

    Adding Controller and OnChanged

--- lib/app/modules/home/todos/todos_widget.dart
+++ lib/app/modules/home/todos/todos_widget.dart
@@ -8,7 +8,28 @@ class TodosWidget extends StatefulWidget {
 }
 
 class _TodosWidgetState extends State<TodosWidget> {
+  final descriptionController = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    descriptionController.addListener(_printDescriptionValue);
+  }
+
+  @override
+  void dispose() {
+    descriptionController.dispose();
+
+    super.dispose();
+  }
+
+  void _printDescriptionValue() {
+    print(
+        'Print value by descriptionController: ${descriptionController.text}');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +40,20 @@ class _TodosWidgetState extends State<TodosWidget> {
           child: Column(
             children: [
               TextFormField(
+                controller: descriptionController,
+                decoration: const InputDecoration(
+                  hintText: 'Description',
+                  border: OutlineInputBorder(),
+                ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter some text';
                   }
 
                   return null;
+                },
+                onChanged: (value) {
+                  print('Print value by onChanged: $value');
                 },
               ),
               ElevatedButton(
