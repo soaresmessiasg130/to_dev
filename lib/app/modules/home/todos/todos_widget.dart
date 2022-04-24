7f4b4866e8da400d9b5e6dbab9b39ddb55830caf
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2022-04-23 21:34:22 -0300

    Requesting Focust to an input

--- lib/app/modules/home/todos/todos_widget.dart
+++ lib/app/modules/home/todos/todos_widget.dart
@@ -10,6 +10,8 @@ class TodosWidget extends StatefulWidget {
 class _TodosWidgetState extends State<TodosWidget> {
   final descriptionController = TextEditingController();
 
+  late FocusNode focusNode;
+
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -17,11 +19,15 @@ class _TodosWidgetState extends State<TodosWidget> {
     super.initState();
 
     descriptionController.addListener(_printDescriptionValue);
+
+    focusNode = FocusNode();
   }
 
   @override
   void dispose() {
     descriptionController.dispose();
+
+    focusNode.dispose();
 
     super.dispose();
   }
@@ -55,13 +61,38 @@ class _TodosWidgetState extends State<TodosWidget> {
                 onChanged: (value) {
                   print('Print value by onChanged: $value');
                 },
+                autofocus: true,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  focusNode.requestFocus();
+                },
+                child: const Text('Next'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Cash',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+
+                  return null;
+                },
+                onChanged: (value) {
+                  print('Print value by onChanged: $value');
+                },
+                autofocus: true,
+                focusNode: focusNode,
               ),
               ElevatedButton(
                 onPressed: () {
                   if (_formKey.currentState!.validate()) {
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(
-                        content: Text('Processing data...'),
+                        content: Text('Done'),
                       ),
                     );
                   }
